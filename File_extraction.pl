768e1a4436dfe5e5260bc715539f77399f42c87b
Dinesh <blitzmnit@gmail.com>
2016-05-01 02:47:06 -0500

    Extract module names and ports included

--- File_extraction.pl
+++ File_extraction.pl
@@ -7,11 +7,22 @@ foreach $line (@contents){
 	foreach $key (keys %hash){
 		if($line =~ m/\b$key\b/i){
 			$hash{$key}++;
-			print "$line\n";
+#			print "$line\n";
 		}
 	}
-} 
+}
+
+#extract module names
+foreach $line (@contents){
+	chomp($line);
+	if(($line =~ m/\bmodule\b\s+(.*?)\((.*?)\);/i) && ($line !~ m/\-+/)){
+		$name = $1;
+		@list = split(/\,/,$2);
+		print $name,"--","@list\n";
+	} 
+}
+
 foreach $key (sort keys %hash){
-	print"$key\t-\t$hash{$key}\n";
+#	print"$key\t-\t$hash{$key}\n";
 }
 close (FILE);
